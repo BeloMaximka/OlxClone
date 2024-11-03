78c8e039a1cbb8f8980d3492374bb298491713e6
BeloMaximka <belomaxim@gmail.com>
2024-11-03 17:34:15 +0200

    Moved functions closer to controllers

--- UsersAPI/src/modules/authentication/controllers/login.ts
+++ UsersAPI/src/modules/authentication/controllers/login.ts
@@ -1,7 +1,10 @@
 import type { Context } from "@oak/oak/context";
-import type { AuthRequest } from "../models/auth-request.ts";
-import { validateCredsAndGetUser } from "../functions/validate-creds-and-get-user.ts";
 import { sendTokens } from "../functions/send-tokens.ts";
+import * as bcrypt from "bcrypt";
+import { Role } from "../../../db/entities/role.ts";
+import { User } from "../../../db/entities/user.ts";
+import { UserPayload } from "../../shared/authentication/models/user-payload.ts";
+import type { AuthRequest } from "../models/auth-request.ts";
 
 export async function login(ctx: Context) {
   const body = (await ctx.request.body.json()) as AuthRequest;
@@ -10,4 +13,18 @@ export async function login(ctx: Context) {
 
   const user = await validateCredsAndGetUser(ctx, body);
   sendTokens(ctx, user);
+}
+
+async function validateCredsAndGetUser(ctx: Context, req: AuthRequest) {
+  const user = await User.findOne({
+    where: {
+      email: req.email,
+    },
+    include: [Role],
+  });
+
+  ctx.assert(user, 401);
+  ctx.assert(await bcrypt.compare(req.password, user.password), 401);
+
+  return new UserPayload(user);
 }
--- UsersAPI/src/modules/authentication/controllers/refresh-tokens.ts
+++ UsersAPI/src/modules/authentication/controllers/refresh-tokens.ts
@@ -1,10 +1,22 @@
 import type { Context } from "@oak/oak/context";
-import { validateRefreshToken } from "../functions/validate-refresh-token.ts";
-import { getUserFromAccessToken } from "../../shared/authentication/functions/get-user-from-access-token.ts";
 import { sendTokens } from "../functions/send-tokens.ts";
+import jwt from "jsonwebtoken";
+import { environmentVariables } from "../../../config/environment-variables.ts";
+import { getUserFromAccessToken } from "../../shared/authentication/functions/get-user-from-access-token.ts";
 
 export async function refreshTokens(ctx: Context) {
-    await validateRefreshToken(ctx);
-    const user = getUserFromAccessToken(ctx);
-    sendTokens(ctx, user);
+  await validateRefreshToken(ctx);
+  const user = getUserFromAccessToken(ctx);
+  sendTokens(ctx, user);
+}
+
+async function validateRefreshToken(ctx: Context) {
+  const refreshToken = await ctx.cookies.get("refresh_token");
+  ctx.assert(refreshToken, 401, "Refresh token is required");
+
+  try {
+    jwt.verify(refreshToken, environmentVariables.jwt.refreshSecret);
+  } catch {
+    ctx.assert(false, 403, "Invalid refresh token");
+  }
 }
